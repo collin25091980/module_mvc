b477505384d1df5678a8e632ac121c8035ee2b04
Sebastien Collin <sebastien.collin91100@gmail.com>
2023-02-04 13:33:32 +0100

    Chapitre 3.4

--- index.php
+++ index.php
@@ -2,14 +2,24 @@
    // Routeur
    require('controller/controller.php');
 
-   if(isset($_GET['page'])) {
-      if($_GET['page'] == 'acceuil') {
+   try {
+      if(isset($_GET['page'])) {
+         if($_GET['page'] == 'acceuil') {
+            home();
+         }
+         else if($_GET['page'] == 'avis') {
+            testimonials();
+         }
+         else {
+            throw new Exception("Cette page n'existe pas ou a été supprimée.");
+         }
+      }
+      else {
          home();
       }
-      else if($_GET['page'] == 'avis') {
-         testimonials();
-      }
    }
-   else {
-      home();
+   catch(Exception $e) {
+      // die('Erreur : ' . $e->getMessage());
+      $error = $e->getMessage();
+      require('view/errorView.php');
    }
